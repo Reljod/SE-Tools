b24931bc7ea769a8386b22cac55429e1d55fbe19
Reljod Oreta <oretareljod@gmail.com>
2022-05-31 15:18:36 +0000

    enh: improve page layout

--- pages/about.tsx
+++ pages/about.tsx
@@ -6,7 +6,7 @@ type Props = {}
 function About({}: Props) {
   return (
   <Main id="about-page">
-    <div className="flex item-center justify-center">
+    <div className="flex item-center justify-center py-5">
       <h1 className="text-5xl">
         About Page
       </h1>
